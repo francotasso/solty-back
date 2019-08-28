79cf7b6f5fe802cc29e77fbc31ca18b0e511e7ba
francotasso <franco.tasso@unmsm.edu.pe>
2019-08-28 13:40:49 -0500

    duplicate email fixed x3

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -21,15 +21,15 @@ async function editUser(req, res, next) {
     const newUser = req.body;
     const validEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(newUser.email);
     const duplicateEmail = await User.find({ email: newUser.email.trim() });
+    console.log(duplicateEmail);
     const validPhone = /^[9][0-9]{8}$/.test(newUser.phone);
     const { userId } = req.params;
     if (newUser.firstName.trim().length == 0 || newUser.lastName.trim().length == 0 || newUser.email.trim().length == 0 || newUser.gender.trim().length == 0 || newUser.phone.trim().length == 0 || newUser.birthday == null) {
         res.status(500).json({ text: 'Complete todos los campos' });
     } else if (!validEmail) {
         res.status(500).json({ text: 'Ingrese un email válido' });
-    } else if (duplicateEmail) {
-        if (duplicateEmail[0].email != newUser.email)
-            res.status(500).json({ text: 'Ya se encuentra registrado este email' });
+    } else if (duplicateEmail.length > 0 && duplicateEmail[0].email != newUser.email) {
+        res.status(500).json({ text: 'Ya se encuentra registrado este email' });
     } else if (!validPhone) {
         res.status(500).json({ text: 'Ingrese un celular válido' });
     } else {
