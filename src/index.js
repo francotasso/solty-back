0f42db24eea9dbb46c56c6830b946b5411627f6c
Tasso <franco.tasso@unmsm.edu.pe>
2019-12-16 10:42:29 -0500

    removing cookies

--- src/index.js
+++ src/index.js
@@ -48,11 +48,11 @@ app.use(session({
     secret: keys.SESSION.secret,
     resave: false,
     saveUninitialized: false,
-    cookie: {
+    /*cookie: {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 24 * 60 * 60 * 1000
-    }
+    }*/
 }));
 app.use(passport.initialize());
 app.use(passport.session());
