edf0f2c4ec787b3aad53ea22757ca8602ed01db7
Tasso <franco.tasso@unmsm.edu.pe>
2019-12-31 23:02:26 -0500

    update product model

--- src/API/API.js
+++ src/API/API.js
@@ -2,6 +2,7 @@ const API = {
     //url: "http://localhost:8080",
     url: "https://solty.herokuapp.com",
     self: "https://solty-back.herokuapp.com"
+    //self: "http:localhost:3000"
 }
 
 module.exports = {
--- src/models/product.js
+++ src/models/product.js
@@ -7,16 +7,34 @@ const productSchema = new Schema({
     price: { type: Number, required: true },
     stock: { type: Number, required: true },
     gender: { type: String, required: true },
-    category: {type: String, required: true},
+    category: { type: String, required: true },
     image: { type: String, required: true },
     description: { type: String, required: true },
+    discount: {
+        percentage: { type: Number, required: true },
+        status: { type: Boolean, required: true }
+    },
+    discountCodes: [{
+        code: { type: String, required: true },
+        percentage: { type: Number, required: true },
+        status: { type: Boolean, required: true }
+    }],
+    available: {
+        sizes: [{
+            size: { type: String, required: true },
+            colors: [{
+                color: { type: String, required: true },
+                stock: { type: Number, required: true }
+            }]
+        }]
+    },
     details: [{
-        id: {type: Number, required: true},
-        content: {type: String, required: true}
+        id: { type: Number, required: true },
+        content: { type: String, required: true }
     }],
     tips: [{
-        id: {type: Number, required: true},
-        content: {type: String, required: true}
+        id: { type: Number, required: true },
+        content: { type: String, required: true }
     }]
 });
 
