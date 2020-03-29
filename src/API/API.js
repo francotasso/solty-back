e4b8bd26694ae2d3686d37eb698bed1be33f2bf2
Tasso <42650227+francotasso@users.noreply.github.com>
2020-03-29 17:45:55 -0500

    localhost api

--- src/API/API.js
+++ src/API/API.js
@@ -1,9 +1,8 @@
 const API = {
-    //url: "http://localhost:8080",
-    //self: "http:localhost:3000"
-    url: "https://solty.herokuapp.com",
-    self: "https://solty-back.herokuapp.com"
-    //
+    url: "http://localhost:8080",
+    self: "http:localhost:3000"
+    //url: "https://solty.herokuapp.com",
+    //self: "https://solty-back.herokuapp.com"
 }
 
 module.exports = {
