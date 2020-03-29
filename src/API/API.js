a7a90352e9997d3d16c0def081abff709fa898cd
Tasso <42650227+francotasso@users.noreply.github.com>
2020-03-29 00:09:39 -0500

    Update API.js

--- src/API/API.js
+++ src/API/API.js
@@ -1,8 +1,8 @@
 const API = {
-    //url: "http://localhost:8080",
-    url: "https://solty.herokuapp.com",
-    self: "https://solty-back.herokuapp.com"
-    //self: "http:localhost:3000"
+    url: "http://localhost:8080",
+    self: "http:localhost:3000"
+    //url: "https://solty.herokuapp.com",
+    //self: "https://solty-back.herokuapp.com"
 }
 
 module.exports = {
