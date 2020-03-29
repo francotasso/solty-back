6785b7e7c86ef64116b8455720cdcf01ca801b59
Tasso <42650227+francotasso@users.noreply.github.com>
2020-03-29 12:14:00 -0500

    Update oauth.js

--- src/routes/oauth.js
+++ src/routes/oauth.js
@@ -21,11 +21,8 @@ router.get('/auth/google', passport.authenticate('google', {
 //redirect url
 router.get('/auth/google/redirect', passport.authenticate('google'),
     (req, res) => {
-        if(req.hostname === 'localhost') res.redirect('http://localhost:8080/oauth');
-        else {
-            let url = `${API.API.url}/oauth`
-            res.redirect(url);
-        }
+        let url = `${API.API.url}/oauth`
+        res.redirect(url);
     }
 );
 
@@ -35,11 +32,8 @@ router.get('/auth/facebook', passport.authenticate('facebook'));
 //redirect url
 router.get('/auth/facebook/redirect', passport.authenticate('facebook'),
     (req, res) => {
-        if(req.hostname === 'localhost') res.redirect('http://localhost:8080/oauth');
-        else {
-            let url = `${API.API.url}/oauth`
-            res.redirect(url);
-        }
+        let url = `${API.API.url}/oauth`
+        res.redirect(url);
     }
 );
 
