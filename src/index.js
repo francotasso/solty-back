a3f78bb354ebc8524f2a063fb8342fcd4efd173d
francotasso <franco.tasso@unmsm.edu.pe>
2020-09-03 00:05:27 -0500

    comment: cookie

--- src/index.js
+++ src/index.js
@@ -55,11 +55,11 @@ app.use(session({
     secret: keys.SESSION.secret,
     resave: false,
     saveUninitialized: false,
-    cookie: {
+    /* cookie: {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: 15 * 60 * 60 * 1000
-    }
+    } */
 }));
 app.use(passport.initialize());
 app.use(passport.session());
